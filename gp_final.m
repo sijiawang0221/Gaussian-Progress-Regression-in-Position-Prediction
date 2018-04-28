b6957e4998b3908d3fca5d322824ed23dd33b257
Sijia Wang <sijiawang@sijias-mbp.dhcp.wustl.edu>
2018-04-28 15:30:28 -0500

    code

---
+++ gp_final.m
@@ -0,0 +1,16 @@
+datasource = csvread('slice_localization_data.csv', 1, 0);
+randsq = randperm(size(datasource,1));
+xtr = datasource(randsq(1:500), 2:round(end/2));
+ytr = datasource(randsq(1:500), end);
+gprMdl = fitrgp(xtr, ytr, 'FitMethod','sr','BasisFunction','linear',...
+        'ActiveSetMethod','sgma','PredictMethod','fic',...
+        'KernelFunction', 'squaredexponential','KernelParameters',[1 0.5 0.5]);
+gloss = zeros(1,20);
+rmse = zeros(1,20);
+
+for i = 1:20
+    xte = datasource(randsq(11001:11000+20*i), 2:round(end/2));
+    yte = datasource(randsq(11001:11000+20*i), end);
+    gloss(i) = loss(gprMdl, xte, yte);
+    rmse(i) = sqrt(gloss(i)/i*20);
+end
