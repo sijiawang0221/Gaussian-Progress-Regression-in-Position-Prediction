e7222fed97efac77567540c4146b53dd3a18ad22
Sijia Wang <sijiawang@sijias-mbp.dhcp.wustl.edu>
2018-04-28 16:15:48 -0500

    delete files

--- prefetch.m
+++
@@ -1,7 +0,0 @@
-function [xtr, ytr, xte, yte] = prefetch(data)
-    trsize = floor(0.8*size(data,1));
-    xtr = data(1:trsize,1:end-1);
-    ytr = data(1:trsize,end);
-    xte = data(trsize+1:end,1:end-1);
-    yte = data(trsize+1:end,end);
-end
