a8f022a82eb22bdab46563c7f1e60501aeb41204
sijiawang <sijiawang@wustl.edu>
2018-05-01 09:40:43 -0500

    5060

--- gp_final.m
+++ gp_final.m
@@ -17,9 +17,15 @@
 
 
 datasource = csvread('slice_localization_data.csv', 1, 0);
-randsq = randperm(size(datasource,1));
-xtr = datasource(randsq(1:100), 2:end-1);
-ytr = datasource(randsq(1:100), end);
+
+traingdata = datasource(1:40177,:); %70 patient
+randtr = randperm(size(trainingdata,1));
+xtr = datasource(randtr(1:10000), 2:end-1);
+ytr = datasource(randtr(1:10000), end);
+
+testdata = datasourse(40178:end,:);
+randte = randperm(size(testdata,1));
+
 % gprMdl = fitrgp(xtr, ytr, 'FitMethod','sr','BasisFunction','linear',...
 %         'ActiveSetMethod','sgma','PredictMethod','fic',...
 %         'KernelFunction', 'ardsquaredexponential','KernelParameters',[1 0.5 0.5]);
@@ -34,8 +40,8 @@ gloss = zeros(1,20);
 rmse = zeros(1,20);
 
 for i = 1:20
-    xte = datasource(randsq(11001:11000+200*i), 2:end-1);
-    yte = datasource(randsq(11001:11000+200*i), end);
+    xte = testdata(randte(1:200*i), 2:end-1);
+    yte = testdata(randte(1:200*i), end);
     % gloss(i) = loss(gprMdl, xte, yte);
     ypred = predict(gprMdl, xte);  
     rmse(i) = sqrt(mean(yte - ypred).^2);
